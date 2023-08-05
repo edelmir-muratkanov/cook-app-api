0a6a3e580442d1b5a6cc0aa8244e07705b43568b
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-05 14:46:53 +0600

    feat(user): init user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 
 import { AuthModule } from './users/auth/auth.module'
+import { UserModule } from './users/user/user.module'
 
 @Module({
-	imports: [AuthModule],
+	imports: [AuthModule, UserModule],
 })
 export class AppModule {}
