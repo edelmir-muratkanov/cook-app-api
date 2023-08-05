b65dd862a86c050426a499838fc31d7dbfe3c525
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-05 19:23:37 +0600

    docs: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
@@ -8,11 +9,21 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const configService = app.get(ConfigService)
 	const port = configService.get<number>('APP_PORT', 8000)
+	const config = new DocumentBuilder()
+		.setTitle('Cook app')
+		.setDescription('Cook app documentation')
+		.setVersion('1.0')
+		.addBearerAuth()
+		.build()
 
 	app.useGlobalPipes(new ValidationPipe({ transform: true }))
+	const document = SwaggerModule.createDocument(app, config)
+
+	SwaggerModule.setup('api', app, document)
 
 	await app.listen(port, () => {
 		Logger.log(`Server started on port ${port}`)
+		Logger.log(`Swagger documentation avalable at http://localhost:${port}/api`)
 	})
 }
 
