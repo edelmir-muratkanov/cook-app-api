2cf19c7fc73db735c43ea4f6897e9fcc805e27b2
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-05 21:57:16 +0600

    docs: add swagger decorators

--- src/shared/decorators/auth.decorator.ts
+++ src/shared/decorators/auth.decorator.ts
@@ -1,4 +1,5 @@
 import { UseGuards, applyDecorators } from '@nestjs/common'
+import { ApiBearerAuth, ApiUnauthorizedResponse } from '@nestjs/swagger'
 
 import { AdminGuard, JwtGuard } from 'src/users/auth/guards'
 
@@ -6,5 +7,7 @@ import { ROLE } from '../providers/typeorm/entities'
 
 export const Auth = (role = ROLE.GUEST) =>
 	applyDecorators(
+		ApiBearerAuth(),
+		ApiUnauthorizedResponse(),
 		role === ROLE.ADMIN ? UseGuards(JwtGuard, AdminGuard) : UseGuards(JwtGuard),
 	)
