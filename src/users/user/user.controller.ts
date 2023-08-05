4df1ff9e8852cef9fecadd00ab35701d5747f39c
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-05 22:00:06 +0600

    docs: add swagger docs to user endpoints

--- src/users/user/user.controller.ts
+++ src/users/user/user.controller.ts
@@ -1,29 +1,73 @@
-import { Controller, Get, Body, Param, Delete, Put } from '@nestjs/common'
+import {
+	Controller,
+	Get,
+	Body,
+	Param,
+	Delete,
+	Put,
+	Query,
+} from '@nestjs/common'
+import { ApiOkResponse, ApiOperation, ApiTags, OmitType } from '@nestjs/swagger'
 
+import { ApiErrorResponse } from 'src/shared/decorators/api-error-response.decorator'
+import { ApiPaginatedResponse } from 'src/shared/decorators/api-paginated-response.decorator'
+import { Auth } from 'src/shared/decorators/auth.decorator'
+import { CurrentUser } from 'src/shared/decorators/current-user.decorator'
+import { PaginationDto } from 'src/shared/dto/pagination.dto'
 import { ParamIdDto } from 'src/shared/dto/param-id.dto'
+import { User } from 'src/shared/providers/typeorm/entities'
 
 import { UpdateUserDto } from './dto/update-user.dto'
 import { UserService } from './user.service'
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
-
-	@Get()
-	async findAll() {
-		return this.userService.findAll()
+	@Auth()
+	@ApiOperation({ summary: 'Получение профиля' })
+	@ApiOkResponse({ type: User })
+	@ApiErrorResponse()
+	@Get('me')
+	async me(@CurrentUser('id') id: string) {
+		return this.userService.byId(id)
 	}
 
+	@ApiOperation({ summary: 'Получение пользователя' })
+	@ApiOkResponse({ type: User })
+	@ApiErrorResponse()
 	@Get(':id')
 	async findOne(@Param('id') { id }: ParamIdDto) {
 		return this.userService.byId(id)
 	}
 
+	@ApiOperation({ summary: 'Получение всех пользователей' })
+	@ApiPaginatedResponse(
+		OmitType(User, [
+			'comments',
+			'followers',
+			'following',
+			'ratings',
+			'recipes',
+		]),
+	)
+	@ApiErrorResponse()
+	@Get()
+	async findAll(@Query() pagination: PaginationDto) {
+		return await this.userService.findAll(pagination)
+	}
+
+	@ApiOperation({ summary: 'Обновление пользователя' })
+	@ApiOkResponse({ type: User })
+	@ApiErrorResponse()
 	@Put(':id')
 	async update(@Param('id') { id }: ParamIdDto, @Body() dto: UpdateUserDto) {
 		return this.userService.update(id, dto)
 	}
 
+	@ApiOperation({ summary: 'Удаление пользователя' })
+	@ApiOkResponse({ type: Boolean })
+	@ApiErrorResponse()
 	@Delete(':id')
 	async remove(@Param('id') { id }: ParamIdDto) {
 		return this.userService.remove(id)
