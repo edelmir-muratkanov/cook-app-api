d4cb43e52380cc84ab675866a1ab326c96142e5b
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-05 14:49:57 +0600

    feat(recipe): init category

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common'
 
 import { CommentModule } from './feedback/comment/comment.module'
 import { RatingModule } from './feedback/rating/rating.module'
+import { CategoryModule } from './recipes/category/category.module'
 import { RecipeModule } from './recipes/recipe/recipe.module'
 import { AuthModule } from './users/auth/auth.module'
 import { UserModule } from './users/user/user.module'
 
 @Module({
-	imports: [AuthModule, UserModule, RatingModule, CommentModule, RecipeModule],
+	imports: [
+		AuthModule,
+		UserModule,
+		RatingModule,
+		CommentModule,
+		RecipeModule,
+		CategoryModule,
+	],
 })
 export class AppModule {}
