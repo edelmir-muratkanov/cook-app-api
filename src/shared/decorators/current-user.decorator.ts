f4534b9b1fa7a144d09d0cdd87b5b6ff39a1d4ff
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-05 19:12:24 +0600

    feat(user): add current user decorator to access user from token

---
+++ src/shared/decorators/current-user.decorator.ts
@@ -0,0 +1,12 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common'
+
+import { User } from '../providers/typeorm/entities'
+
+export const CurrentUser = createParamDecorator(
+	(data: keyof User, ctx: ExecutionContext) => {
+		const req = ctx.switchToHttp().getRequest<{ user: User }>()
+		const user = req.user
+
+		return data ? user[data] : user
+	},
+)
