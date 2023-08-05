e55ff935479cf508156042276ad669560acab60d
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-05 21:58:21 +0600

    docs: add pagination decorator

---
+++ src/shared/decorators/api-paginated-response.decorator.ts
@@ -0,0 +1,30 @@
+import { Type, applyDecorators } from '@nestjs/common'
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger'
+
+import { PaginatedDto } from '../dto/pagination.dto'
+
+export const ApiPaginatedResponse = <TModel extends Type<any>>(
+	model: TModel,
+) => {
+	return applyDecorators(
+		ApiExtraModels(model),
+		ApiOkResponse({
+			schema: {
+				allOf: [
+					{ $ref: getSchemaPath(PaginatedDto) },
+					{
+						properties: {
+							count: {
+								type: 'number',
+							},
+							data: {
+								type: 'array',
+								items: { $ref: getSchemaPath(model) },
+							},
+						},
+					},
+				],
+			},
+		}),
+	)
+}
