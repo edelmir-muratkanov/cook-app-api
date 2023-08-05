ea0a8a91425644b2c83b8aa300a095857d345966
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-05 15:25:43 +0600

    feat: update start by using env port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	const configService = app.get(ConfigService)
+	const port = configService.get<number>('APP_PORT', 8000)
+
+	await app.listen(port, () => {
+		Logger.log(`Server started on port ${port}`)
+	})
 }
 
 void bootstrap()
