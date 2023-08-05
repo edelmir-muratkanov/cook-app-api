b67d519528163bc5fe1907581a43d07dc31acf21
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-05 14:47:56 +0600

    feat(rating): init rating

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 
+import { RatingModule } from './feedback/rating/rating.module'
 import { AuthModule } from './users/auth/auth.module'
 import { UserModule } from './users/user/user.module'
 
 @Module({
-	imports: [AuthModule, UserModule],
+	imports: [AuthModule, UserModule, RatingModule],
 })
 export class AppModule {}
