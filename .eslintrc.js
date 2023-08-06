af450b71aea7633c9df9776858741f991bcab90d
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-06 18:09:52 +0600

    style: remove rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,79 +1,79 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'prettier', 'jest'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:jest/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  settings: {
-    'import/resolver': {
-      typescript: {},
-    },
-  },
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'import/order': [
-      'error',
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'unknown',
-          ['parent', 'sibling'],
-          'index',
-          'object',
-          'type',
-        ],
-        pathGroups: [
-          {
-            pattern: '@/**',
-            group: 'internal',
-            position: 'after',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['react'],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
-    'import/no-extraneous-dependencies': 'off',
-    'import/no-restricted-paths': [
-      'error',
-      {
-        zones: [
-          {
-            target: ['src/pages', 'src/app'],
-            from: 'src/modules/**/!(index.ts)',
-          },
-          {
-            target: ['src/modules', 'src/pages', 'src/app'],
-            from: 'src/shared/**/!(index.ts)',
-          },
-        ],
-      },
-    ],
-  },
-};
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint/eslint-plugin', 'import', 'prettier', 'jest'],
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+		// 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+		'plugin:jest/recommended',
+	],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	ignorePatterns: ['.eslintrc.js'],
+	settings: {
+		'import/resolver': {
+			typescript: {},
+		},
+	},
+	rules: {
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'import/order': [
+			'error',
+			{
+				groups: [
+					'builtin',
+					'external',
+					'internal',
+					'unknown',
+					['parent', 'sibling'],
+					'index',
+					'object',
+					'type',
+				],
+				pathGroups: [
+					{
+						pattern: '@/**',
+						group: 'internal',
+						position: 'after',
+					},
+				],
+				pathGroupsExcludedImportTypes: ['react'],
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+			},
+		],
+		'import/no-extraneous-dependencies': 'off',
+		'import/no-restricted-paths': [
+			'error',
+			{
+				zones: [
+					{
+						target: ['src/pages', 'src/app'],
+						from: 'src/modules/**/!(index.ts)',
+					},
+					{
+						target: ['src/modules', 'src/pages', 'src/app'],
+						from: 'src/shared/**/!(index.ts)',
+					},
+				],
+			},
+		],
+	},
+}
