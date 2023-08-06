e193e1d049bbac21f3e0b85f17c00a8c4a3c262e
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-06 17:44:06 +0600

    docs: update auth decorator 401 exception response

--- src/shared/decorators/auth.decorator.ts
+++ src/shared/decorators/auth.decorator.ts
@@ -3,11 +3,12 @@ import { ApiBearerAuth, ApiUnauthorizedResponse } from '@nestjs/swagger'
 
 import { AdminGuard, JwtGuard } from 'src/users/auth/guards'
 
+import { ErrorResponseDto } from '../dto/error-response.dto'
 import { ROLE } from '../providers/typeorm/entities'
 
 export const Auth = (role = ROLE.GUEST) =>
 	applyDecorators(
 		ApiBearerAuth(),
-		ApiUnauthorizedResponse(),
+		ApiUnauthorizedResponse({ type: ErrorResponseDto }),
 		role === ROLE.ADMIN ? UseGuards(JwtGuard, AdminGuard) : UseGuards(JwtGuard),
 	)
