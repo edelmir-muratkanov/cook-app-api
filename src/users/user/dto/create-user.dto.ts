820657131bea2b8da9817e4ae10f41c5f68b70e4
Edelmir Muratkanov <muratkanov02@gmail.com>
2023-08-06 16:14:46 +0600

    docs: add property manually

--- src/users/user/dto/create-user.dto.ts
+++ src/users/user/dto/create-user.dto.ts
@@ -1,17 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator'
 
 export class CreateUserDto {
-	/** Электронная почта пользователя */
+	@ApiProperty({
+		description: 'Электронная почта пользователя',
+		default: 'admin@admin.com',
+	})
 	@IsEmail()
 	@IsNotEmpty()
 	email: string
 
-	/** Имя пользователя пользователя */
+	@ApiProperty({
+		description: 'Имя пользователя пользователя',
+		default: 'admin',
+	})
 	@IsString()
 	@IsNotEmpty()
 	username: string
 
-	/** Пароль пользователя */
+	@ApiProperty({ description: 'Пароль пользователя', default: '123456' })
 	@IsNotEmpty()
 	@MinLength(6)
 	password: string
--- src/users/user/dto/update-user.dto.ts
+++ src/users/user/dto/update-user.dto.ts
@@ -1,9 +1,10 @@
-import { PartialType } from '@nestjs/swagger'
+import { ApiProperty, PartialType } from '@nestjs/swagger'
 import { IsOptional, IsString } from 'class-validator'
 
 import { CreateUserDto } from './create-user.dto'
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+	@ApiProperty({ description: 'Аватарка пользователя' })
 	@IsString()
 	@IsOptional()
 	image: string
